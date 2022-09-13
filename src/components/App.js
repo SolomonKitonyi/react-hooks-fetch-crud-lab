e6b106b57f0434fdba6bb2b1348cbff9c5d51d1f
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-09-13 12:07:04 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,15 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-
+  const [questions,setQuestions] = useState([])
+  function getData(newQuestion) {
+    const newQuestions = [...questions,newQuestion]
+    setQuestions(newQuestions)
+  }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm getData={getData} /> : <QuestionList questions={questions} setQuestions={setQuestions}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions,setQuestions}) {
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(data => {
+      setQuestions(data)
+    })
+  },[questions])
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{
+        questions.map(question => (<QuestionItem question={question} key={question.id} />))
+      /* display QuestionItem components here after fetching */}</ul>
     </section>
   );
 }
